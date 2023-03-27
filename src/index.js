9d904c2ef0cde9ccf3c84033e0ad5c65115b5dd8
= <=>
2023-03-27 23:19:54 +0700

    socket io p1

--- src/index.js
+++ src/index.js
@@ -7,14 +7,22 @@ const port = 3000
 // config view engine
 configViewEngine(app);
 
+// socket.io
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => { 
+    console.log('Co nguoi ket noi: ', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Nguoi nay da ngat ket noi: ', socket.id)
+    })
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World Tien nha hihi!')
+    return res.render('test')
 })
 
-app.get('/hi', (req, res) => {
-    return res.render('test')
-  })
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
